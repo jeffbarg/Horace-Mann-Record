389a280a0f95f70319b71e0cab9ab2740e2ad617
Jeff Barg <jeffbarg@fructosetech.com>
2012-07-23 01:56:30 -0400

    asdf

--- django/Record/Record/urls.py
+++ django/Record/Record/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Record.views.home', name='home'),
-    # url(r'^Record/', include('Record.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'articles.views.index'),
+	url(r'^articles/(?P<article_id>\d+)/$', 'articles.views.detail'),
+	
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
