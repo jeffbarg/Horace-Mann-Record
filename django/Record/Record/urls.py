ced7168ecc89db33cf91d73617296cdd35551c7c
root <root@jeffbarg.com>
2012-07-22 23:17:57 -0700

    stuf

--- django/Record/Record/urls.py
+++ django/Record/Record/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+
 	url(r'^$', 'articles.views.index'),
 	url(r'^articles/(?P<article_id>\d+)/$', 'articles.views.detail'),
 	
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
 )
--- django/Record/django.wsgi
+++ django/Record/django.wsgi
@@ -2,7 +2,7 @@ import os, sys
 
 sys.path.append('var/www/record.jeffbarg.com/django')
 sys.path.append('/var/www/record.jeffbarg.com/django/Record')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'record.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Record.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
