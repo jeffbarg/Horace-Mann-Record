42684f184e27e90488f7e3ea131bb8e543878908
Jeff Barg <jeffbarg@fructosetech.com>
2012-07-27 14:23:44 -0400

    Made author editing AWESOME…

--- django/Record/articles/admin.py
+++ django/Record/articles/admin.py
@@ -6,10 +6,20 @@ class ArticleAdmin(admin.ModelAdmin):
     filter_vertical = ('authors',)
 	#inlines = [ArticleTagInline]
     class Media:
-		js = ('js/tiny_mce/tiny_mce.js', 'js/appmedia/textareas.js',)
+
+		js = ('js/tiny_mce/tiny_mce.js',
+			 'js/appmedia/textareas.js',)
 	
 	
-	
+class AuthorAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, 
+			{ 'fields' : ('username', 'first_name', 'last_name',) }), 
+		('Optional Info',
+			{'fields' : ('bio', 'profile_picture'),
+			 'classes': ('collapse',),}),
+		)
+
 
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
