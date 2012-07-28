fb6ac40aefd42508872f80ba35539884a15bf674
Jeff Barg <jeffbarg@fructosetech.com>
2012-07-28 17:10:21 -0400

    changed layout to swag

--- django/Record/articles/views.py
+++ django/Record/articles/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_article_list = Article.objects.all().order_by('-date_published')[:5]
-    t = loader.get_template('index.html')
-    c = Context({
+    t = loader.get_template('articles/index.html')
+    c = RequestContext(request, {
         'latest_article_list': latest_article_list,
     })
     return HttpResponse(t.render(c))
