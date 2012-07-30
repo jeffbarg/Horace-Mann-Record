0d2444dcb083e1ae46cfffeaa70ab4ff03143aa2
Jeff Barg <jeffbarg@fructosetech.com>
2012-07-30 11:46:29 -0400

    **

--- django/Record/articles/search_indexes.py
+++ django/Record/articles/search_indexes.py
@@ -6,11 +6,11 @@ from articles.models import Article
 class NoteIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     author = indexes.CharField(model_attr='user')
-    pub_date = indexes.DateTimeField(model_attr='pub_date')
+    date_published = indexes.DateTimeField(model_attr='date_published')
 
     def get_model(self):
         return Article
 
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+        return self.get_model().objects.filter(date_published__lte=datetime.datetime.now())
