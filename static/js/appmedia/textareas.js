4acdea37f97ec06c109240ebf89b38ca50682c7e
Jeff Barg <jeffbarg@fructosetech.com>
2012-08-31 22:01:43 -0400

    paste-as-plain-text fix hopefully

--- static/js/appmedia/textareas.js
+++ static/js/appmedia/textareas.js
@@ -1,33 +1,11 @@
-function setPlainText() {
-    var ed = tinyMCE.get('elm1');
-
-    ed.pasteAsPlainText = true;  
-
-    //adding handlers crossbrowser
-    if (tinymce.isOpera || /Firefox\/2/.test(navigator.userAgent)) {
-        ed.onKeyDown.add(function (ed, e) {
-            if (((tinymce.isMac ? e.metaKey : e.ctrlKey) && e.keyCode == 86) || (e.shiftKey && e.keyCode == 45))
-                ed.pasteAsPlainText = true;
-        });
-    } else {            
-        ed.onPaste.addToTop(function (ed, e) {
-            ed.pasteAsPlainText = true;
-        });
-    }
-}
 
 
 tinyMCE.init({
 	mode : "textareas",
 	theme : "advanced",
 
-plugins : "paste",
-paste_text_sticky : true,
-setup : function(ed) {
-    ed.onInit.add(function(ed) {
-      ed.pasteAsPlainText = true;
-    });
-  }
+
+
 
 	//content_css : "/appmedia/blog/style.css",
 	theme_advanced_toolbar_location : "top",
@@ -38,7 +16,7 @@ setup : function(ed) {
 	height:"320",
 	theme_advanced_path : false,
 	auto_cleanup_word : true,
-	plugins : "table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,searchreplace,print,contextmenu,fullscreen",
+	plugins : "table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,searchreplace,print,contextmenu,fullscreen, paste",
 	plugin_insertdate_dateFormat : "%m/%d/%Y",
 	plugin_insertdate_timeFormat : "%H:%M:%S",
 	extended_valid_elements : "a[name|href|target=_blank|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
@@ -47,6 +25,13 @@ setup : function(ed) {
 		theme_advanced_buttons1 : "fullscreen,separator,preview,separator,cut,copy,paste,separator,undo,redo,separator,search,replace,separator,code,separator,cleanup,separator,bold,italic,underline,strikethrough,separator,forecolor,backcolor,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,help",
 		theme_advanced_buttons2 : "removeformat,styleselect,formatselect,fontselect,fontsizeselect,separator,bullist,numlist,outdent,indent,separator,link,unlink,anchor",
 		theme_advanced_buttons3 : "sub,sup,separator,image,insertdate,inserttime,separator,tablecontrols,separator,hr,advhr,visualaid,separator,charmap,emotions,iespell,flash,separator,print"
-	}
+	},
+
+	//Keeps Paste Text feature active until user deselects the Paste as Text button
+	paste_text_sticky: true,
+	paste_auto_cleanup_on_paste : true,
+	paste_remove_styles: true,
+	paste_remove_styles_if_webkit: true,
+	paste_strip_class_attributes: true,
 });
 
