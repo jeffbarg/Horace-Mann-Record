9c7bc920ff621e76f6c056d8810f8c009ff9787a
Jeff Barg <jeffbarg@fructosetech.com>
2012-09-01 01:00:16 -0400

    **

--- django/Record/issues/views.py
+++ django/Record/issues/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.template import RequestContext, loader
 from issues.models import Issue
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import Http404
 
